1a6afdf0cb942fa1c4e7e94fa81ada22fec78d0c
JaeHyeon <njonhj@naver.com>
2022-03-26 23:21:39 +0900

    IntelliJ Setting and HackerRank Java Practice (2 questions) 1. HelloJava 2. UsingScanner

---
+++ src/HackerRank/HelloJava.java
@@ -0,0 +1,9 @@
+package HackerRank;
+
+public class HelloJava {
+    public static void main (String[] args){
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+        // System.out.println 콘솔 창에 문자열 띄우기.
+    }
+}
